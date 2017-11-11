08c79826c1f38ac487cb21b5b19bc649b62f3e5c
Alexis <will1397@purdue.edu>
2017-11-10 22:13:00 -0500

    Added sign up form

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 	res.sendFile('/public/MainPage.html', {root: __dirname})
 });
+app.post('/SignUp.html', function(req, res) {
+	res.sendFile('/public/SignUp.html', {root: __dirname})
+});
 var port = 8080; // you can use any port
 app.listen(port);
 console.log('Listening on: ' + port);
