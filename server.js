4c26ada618843225d761c1314d151fabedd05749
Alexis <will1397@purdue.edu>
2017-11-11 23:52:16 -0500

    Added code to get form data from signup page, also deleted example .js files

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 	res.sendFile('/public/MainPage.html', {root: __dirname})
 });
 app.post('/SignUp.html', function(req, res) {
-	res.sendFile('/public/SignUp.html', {root: __dirname})
+	res.send('Username: ' + req.body.user_name);
 });
 var port = 8080; // you can use any port
 app.listen(port);
