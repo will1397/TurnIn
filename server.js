660d0c7a5e061edc8cae9b5dc08b6ebc45c3b117
Alexis <will1397@purdue.edu>
2017-11-12 21:12:32 -0500

    Added login, changed navbar layout, and added extra dependencies

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var passport = require('passport');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
 	res.sendFile('/public/MainPage.html', {root: __dirname})
 });
-app.post('/SignUp.html', function(req, res) {
-	res.send('Username: ' + req.body.user_name);
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('a user disconnected');
+	});
 });
+
+app.post('/SignUp.html', function(req, res) {
+	res.send('Username: ' + req.params.user_name); //test
+	
+	if (req.body.user_name && req.body.user_password) {
+		//check if username and password can be used with call to MySQL DB
+	}
+});
+
 var port = 8080; // you can use any port
 app.listen(port);
 console.log('Listening on: ' + port);
